4a21ad3271f313e771ad4af340c918fae237d83f
Russell Goualin <r.goualin@hotmail.com>
2019-09-25 12:53:23 +0100

    category model

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
 
 // app
 const app = express();
@@ -34,6 +35,7 @@ app.use(expressValidator());
 // route middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000
 
