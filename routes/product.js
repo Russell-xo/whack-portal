480aa03e18c30ebe0b9605d78f0e3d4b90453293
Russell Goualin <r.goualin@hotmail.com>
2019-10-01 12:09:44 +0100

    product get photo

--- routes/product.js
+++ routes/product.js
@@ -5,16 +5,18 @@ const { create, productById, read, remove, update, list, listRelated, listCatego
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById }= require('../controllers/user');
 
-router.get('/product/photo/:productId', photo)
 router.get('/product/:productId', read)
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove )
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update )
 
-router.post("/products/by/search", listBySearch);
+
 router.get('/products', list)
 router.get('/products/related/:productId', listRelated)
 router.get('/products/categories', listCategories)
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo)
+
 
 router.param('userId', userById)
 router.param('productId', productById)
