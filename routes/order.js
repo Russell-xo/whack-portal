6e44cc5f9eec88c20e360c4c152957d0990c1d04
Russell Goualin <r.goualin@hotmail.com>
2019-12-20 11:19:44 +0000

    bug on status link

--- routes/order.js
+++ routes/order.js
@@ -15,7 +15,7 @@ const { decreaseQuantity} = require('../controllers/product');
 router.post('/order/create/:userId', requireSignin, isAuth, addOrderToUserHistory, decreaseQuantity, create)
 
 router.get('/order/list/:userId', requireSignin, isAuth, isAdmin, listOrders)
-router.get('/order/status-value/:userId', requireSignin, isAuth, isAdmin, getStatusValues)
+router.get('/order/status-values/:userId', requireSignin, isAuth, isAdmin, getStatusValues)
 
 
 router.param('userId', userById)
